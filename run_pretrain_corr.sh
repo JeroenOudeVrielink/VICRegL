16a867f7dd40a139a5afa065f574b19ef485bb23
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-05-31 17:28:56 +0930

    adding stuff

---
+++ run_pretrain_corr.sh
@@ -0,0 +1,21 @@
+
+python main_vicregl.py \
+--fp16 \
+--exp-dir /data/vicregl_models \
+--arch resnet50 \
+--epochs 300 \
+--batch-size 128 \
+--optimizer lars \
+--base-lr 0.3 \
+--weight-decay 1e-06 \
+--size-crops 224 \
+--num-crops 2 \
+--min_scale_crops 0.08 \
+--max_scale_crops 1.0 \
+--alpha 0.75 \
+--num-workers 10 \
+--exp_name "vicregl_bs128_cov_coeff_10" \
+--data_path /data/AIML_rot_corrected \
+--log-tensors-interval 100 \
+--checkpoint-freq 20 \
+--cov-coeff 10.0
